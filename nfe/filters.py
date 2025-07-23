365c96fe3e00fee281f2460b14664ee40b3c86c4
André Camargo <andre.camargo@msn.com>
2025-07-23 10:05:45 -0300

    Adicionando condição para filtro através do OR nas pesquisas de produtos

--- nfe/filters.py
+++ nfe/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 from .models import NotaFiscal, Produto, Emitente
 
 
@@ -12,18 +13,30 @@ class NotaFiscalFilter(django_filters.FilterSet):
 
 
 class ProdutoFilter(django_filters.FilterSet):
+    q = django_filters.CharFilter(method='filter_by_q', label="Pesquisar")
     cProd = django_filters.CharFilter(field_name='cProd', lookup_expr='icontains')
     xProd = django_filters.CharFilter(field_name='xProd', lookup_expr='icontains')
+    cEAN = django_filters.CharFilter(field_name='cEAN', lookup_expr='icontains')
 
     class Meta:
         model = Produto
-        fields = ['cProd', 'xProd', 'nItem']
+        fields = ['cProd', 'xProd', 'cEAN']
+
+    def filter_by_q(self, queryset, name, value):
+        if value:
+            # Usando Q para aplicar OR entre as colunas
+            return queryset.filter(
+                Q(cProd__icontains=value) |
+                Q(xProd__icontains=value) |
+                Q(cEAN__icontains=value)
+            )
+        return queryset
 
 
 class FornecedorFilter(django_filters.FilterSet):
     cnpj = django_filters.CharFilter(field_name='CNPJ', lookup_expr='icontains')
     xNome = django_filters.CharFilter(field_name='xNome', lookup_expr='icontains')
-    
+
     class Meta:
         model = Emitente
         fields = ['CNPJ', 'xNome']
