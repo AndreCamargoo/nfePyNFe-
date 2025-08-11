85295853de49c4e353f4cc9a583d8de81bc97e63
André Camargo <andre.camargo@msn.com>
2025-08-10 22:14:50 -0300

    Correcao na url rotas apex finalziadas

--- apexcharts/urls.py
+++ apexcharts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('apexcharts/custo-medio-fornecedo/', views.CustoMedioFornecedorAPIView.as_view(), name='apexcharts-custo-medio-fornecedor'),
+    path('apexcharts/custo-medio-fornecedor/', views.CustoMedioFornecedorAPIView.as_view(), name='apexcharts-custo-medio-fornecedor'),
     path('apexcharts/participacao-fornecedores/', views.ParticipacaoFornecedoresAPIView.as_view(), name='apexcharts-participacao-fornecedores'),
     path('apexcharts/concentracao-produtos/', views.ConcentracaoProdutosAPIView.as_view(), name='apexcharts-concentracao-produtos'),
     path('apexcharts/frequencia-compras/', views.FrequenciaComprasAPIView.as_view(), name='apexcharts-frequencia-compras'),
