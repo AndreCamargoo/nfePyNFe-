b5c9901224462c139b1b02ad9c8a5195b6529952
André Camargo <andre.camargo@msn.com>
2026-04-07 11:11:24 -0300

    Removendo agenda e cloud

--- cloud/segmento/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Segmento
-
-
-class SegmentoModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Segmento
-        fields = '__all__'
