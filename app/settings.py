aa0dbc413ef32b1dc0300e197ec649089a4ef0b1
André Camargo <andre.camargo@msn.com>
2026-05-29 15:15:28 -0300

    Permitindo Cors para sorteio subdominio

--- app/settings.py
+++ app/settings.py
@@ -90,6 +90,9 @@ else:
 
         'https://moneyupgestao.com.br',
         'https://www.moneyupgestao.com.br',
+
+        'https://sorteio.azv.tec.br/',
+        'https://www.sorteio.azv.tec.br/',
     ]
     CORS_ALLOW_CREDENTIALS = True  # Permitir credenciais (por exemplo, cookies ou JWTs)
 
@@ -113,6 +116,9 @@ else:
 
         'https://moneyupgestao.com.br',
         'https://www.moneyupgestao.com.br',
+
+        'https://sorteio.azv.tec.br/',
+        'https://www.sorteio.azv.tec.br/',
     ]
 
 # Cabeçalhos permitidos para CORS
